65d22eff6b7ac1180ce4010231e9374e209cac3a
OwenB7 <wowzuh62@gmail.com>
2018-10-31 15:57:37 -0400

    changing the main class

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -3,10 +3,44 @@ package com.mygdx.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
+import com.packtpub.libgdx.ghostrunner.GhostRunnerMain;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.
+Settings;
 
-public class DesktopLauncher {
-	public static void main (String[] arg) {
+/**
+ * edited by Owen Burnham in Assignment 3
+ * This is the main class for the GhostRunner-Desktop project 
+ */
+public class DesktopLauncher
+{
+	private static boolean rebuildAtlas = false;
+	private static boolean drawDebugOutline = true;
+	
+	/**
+	 * Edited by Owen Burnham in Assignment 3
+	 * This is the main method that runs the project
+	 * @param arg
+	 */
+	public static void main (String[] arg) 
+	{
+		if (rebuildAtlas)
+		{
+			Settings settings = new Settings();
+			settings.maxWidth = 1024;
+			settings.maxHeight = 1024;
+			settings.duplicatePadding = false;
+			settings.debug = drawDebugOutline;
+			
+			/* settings, input, output, packfilename */
+			TexturePacker.process(settings, "assets-raw/images", "../core/assets/images", "canyonbunny.pack");
+		}
+		
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
+		config.title = "CanyonBunny";
+		config.width = 800;
+		config.height = 480;
+		new LwjglApplication(new GhostRunnerMain(), config);
 	}
 }
+
