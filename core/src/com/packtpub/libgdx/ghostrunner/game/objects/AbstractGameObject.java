64077726813b95b8fddafaae73a7d1863d794eab
OwenB7 <wowzuh62@gmail.com>
2018-11-05 14:36:18 -0500

    Added AbstractGameObject class

---
+++ core/src/com/packtpub/libgdx/ghostrunner/game/objects/AbstractGameObject.java
@@ -0,0 +1,46 @@
+package com.packtpub.libgdx.ghostrunner.game.objects;
+ 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+/**
+ * This class will hold all the common attributes
+ * and fucntionalities that each of our game objects
+ * inherit from
+ */
+public abstract class AbstractGameObject
+{
+    public Vector2 position;
+    public Vector2 dimension;
+    public Vector2 origin;
+    public Vector2 scale;
+    public float rotation;
+    
+    /**
+     * Makes instances of all the variables for the
+     * abstract game object 
+     */
+    public AbstractGameObject()
+    {
+        position = new Vector2();
+        dimension = new Vector2(1, 1);
+        origin = new Vector2();
+        scale = new Vector2(1, 1);
+        rotation = 0;
+    }
+    
+    /**
+     * @param deltaTime
+     * updates objects accordingly in relevance
+     * to the delta time
+     */
+    public void update(float deltaTime)
+    {
+        
+    }
+    
+    /**
+     * @param batch
+     * objects are rendered accordingly
+     */
+    public abstract void render(SpriteBatch batch);
+}
