c3e870be4302704f642f7b1916a89fffd473a356
OwenB7 <wowzuh62@gmail.com>
2018-11-06 12:40:39 -0500

    Appropriate changes made to CameraHelper class to change from Sprites to AbstractGameObject

--- core/src/com/mygdx/game/CameraHelper.java
+++ core/src/com/mygdx/game/CameraHelper.java
@@ -1,9 +1,9 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.packtpub.libgdx.ghostrunner.game.objects.AbstractGameObject;
 
 /**
  * This class assists us with managing and 
@@ -18,7 +18,7 @@ public class CameraHelper
     private Vector2 position;
     private float zoom;
     
-    private Sprite target;
+    private AbstractGameObject target;
     
     /**
      * instance of camera helper
@@ -37,8 +37,8 @@ public class CameraHelper
     public void update (float deltaTime)
     {
     	if (!hasTarget()) return;
-    	position.x = target.getX() + target.getOriginX();
-    	position.y = target.getY() + target.getOriginY();
+    	position.x = target.position.x + target.origin.x;
+    	position.y = target.position.y + target.origin.y;
     }
     
     /**
@@ -89,7 +89,7 @@ public class CameraHelper
      * sets the target
      * @param target
      */
-    public void setTarget (Sprite target)
+    public void setTarget (AbstractGameObject target)
     {
         this.target = target;
     }
@@ -97,7 +97,7 @@ public class CameraHelper
     /**
      * returns the target
      */
-    public Sprite getTarget ()
+    public AbstractGameObject getTarget ()
     {
         return target;
     }
@@ -111,9 +111,9 @@ public class CameraHelper
         return target != null;
     }
     
-    public boolean hasTarget (Sprite target)
+    public boolean hasTarget (AbstractGameObject target)
     {
-    return hasTarget() && this.target.equals(target);
+    	return hasTarget() && this.target.equals(target);
     }
     
     /** 
